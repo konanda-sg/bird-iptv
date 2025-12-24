7aa3b6c413bdb56d9d2727713bda09da5518f828
birdtwelve <dapotinuoye@gmail.com>
2025-12-24 11:35:07 +0000

    Update PlutoTV URL in fetch.py

--- EPG/fetch.py
+++ EPG/fetch.py
@@ -38,7 +38,7 @@ epg_urls = [
     "https://epgshare01.online/epgshare01/epg_ripper_US_LOCALS1.xml.gz",
     "https://epgshare01.online/epgshare01/epg_ripper_ZA1.xml.gz",
     "https://i.mjh.nz/Plex/all.xml.gz",
-    "https://i.mjh.nz/PlutoTV/gb.xml.gz",
+    "https://i.mjh.nz/PlutoTV/all.xml.gz",
     "https://i.mjh.nz/Roku/all.xml.gz",
     "https://raw.githubusercontent.com/BuddyChewChew/tubi-scraper/refs/heads/main/tubi_epg.xml",
     "https://raw.githubusercontent.com/BuddyChewChew/xumo-playlist-generator/main/playlists/xumo_epg.xml.gz",
